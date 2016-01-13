9385fe6266ac503301ee39e97cb19aa5252f3c5a
Hans Henrik Gabelgaard <hhg@gabelgaard.org>
2016-01-13 11:25:20 +0100

    Runbot test

--- partner_firstname/__openerp__.py
+++ partner_firstname/__openerp__.py
@@ -32,7 +32,7 @@
     'data': [
         'views/res_partner.xml',
         'views/res_user.xml',
-        'data/res_partner.yml',
+        #'data/res_partner.yml',
     ],
     'demo': [],
     'test': [],
