055a146aef8495ddf2e640caf9ddfa5e188dbaca
Hans Henrik Gabelgaard <hhg@gabelgaard.org>
2018-06-12 13:40:03 +0200

    We don't need this module

--- partner_second_lastname/tests/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-# © 2015 Grupo ESOC Ingeniería de Servicios, S.L.U.
-
-from . import test_name, test_onchange
-from openerp.addons.partner_firstname.tests import test_empty
